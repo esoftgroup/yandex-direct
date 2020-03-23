3f237234c33bdcaaa9c07b03804ec9daad79b9c0
igorfominyh <igor-fominykh@yandex.ru>
2020-03-24 00:00:33 +0300

    1

--- src/Campaigns.php
+++ src/Campaigns.php
@@ -11,10 +11,79 @@
 // namespace HelloComposer;
 namespace Esoftgroup\YandexDirect;
 
-class Campaigns
+class Campaigns extends Config
 {
-    public function get_campaigns()
+    public function get_campaigns($connect = array())
     {
-        return array(1,2,3);
+        //--- Входные данные ----------------------------------------------------//
+        // Адрес сервиса Campaigns для отправки JSON-запросов (регистрозависимый)
+        $url = $this->api_url.'campaigns';
+        
+        // OAuth-токен пользователя, от имени которого будут выполняться запросы
+        $token = $connect['access_token'];
+        // Логин клиента рекламного агентства
+        // Обязательный параметр, если запросы выполняются от имени рекламного агентства
+        $clientLogin = $connect['client_login'];
+
+        //--- Подготовка и выполнение запроса -----------------------------------//
+        // Установка HTTP-заголовков запроса
+        $headers = array(
+            "Authorization: Bearer $token",                    // OAuth-токен. Использование слова Bearer обязательно
+            "Client-Login: $clientLogin",                      // Логин клиента рекламного агентства
+            "Accept-Language: ru",                             // Язык ответных сообщений
+            "Content-Type: application/json; charset=utf-8"    // Тип данных и кодировка запроса
+        );
+
+        // Параметры запроса к серверу API Директа
+        $params = array(
+            'method' => 'get',                                 // Используемый метод сервиса Campaigns
+            'params' => array(
+                'SelectionCriteria' => (object) array(
+                    //'States' => array('ON'),
+                ),        // Критерий отбора кампаний. Для получения всех кампаний должен быть пустым
+                'FieldNames' => array("BlockedIps", "ExcludedSites", "Currency", "DailyBudget", "Notification", "EndDate", "Funds", "ClientInfo", "Id", "Name", "NegativeKeywords", "RepresentedBy", "StartDate", "Statistics", "State", "Status", "StatusPayment", "StatusClarification", "SourceId", "TimeTargeting", "TimeZone", "Type"),             // Названия параметров, которые требуется получить
+                'TextCampaignFieldNames' => array("CounterIds", "RelevantKeywords", "Settings", "BiddingStrategy", "PriorityGoals"),
+                // Выборка
+                'Page' => array(
+                    'Limit' => 10000,
+                    'Offset' => 0,
+                ),
+            )
+        );
+
+        // Преобразование входных параметров запроса в формат JSON
+        //$body = json_encode($params, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+        $body = json_encode($params);
+
+        // Создание контекста потока: установка HTTP-заголовков и тела запроса
+        $streamOptions = stream_context_create(array(
+            'http' => array(
+                'method' => 'POST',
+                'header' => $headers,
+                'content' => $body
+            ),
+            'ssl' => array(
+                'verify_peer' => false,
+                'verify_peer_name' => false,
+            ),
+            // Для полноценного использования протокола HTTPS можно включить проверку SSL-сертификата сервера API Директа
+            /*'ssl' => array(
+               'verify_peer' => true,
+               'cafile' => getcwd().DIRECTORY_SEPARATOR.'CA.pem' // Путь к локальной копии корневого SSL-сертификата
+            )*/
+        ));
+
+        // Выполнение запроса, получение результата
+        $result = file_get_contents($url, 0, $streamOptions);
+        //print_r(json_decode($result, true));
+
+        $campaigns = json_decode($result, true);
+        //print_r($campaigns);
+
+        if($campaigns['error']) {
+            return $campaigns['error']['error_code']." - ".$campaigns['error']['error_detail'].": ".$campaigns['error']['error_string'];
+        } else {
+            return $campaigns['result']['Campaigns'];
+        }
     }
 }
---
+++ src/Config.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Esoftgroup\YandexDirect;
+
+class Config
+{
+    public $api_url = 'https://api.direct.yandex.com/json/v5/';
+    public $api_sandbox_url = 'https://api-sandbox.direct.yandex.com/json/v5/';
+
+}
